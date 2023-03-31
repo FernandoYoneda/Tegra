75d673e97ca6de0c5f59302e56af7c6a877578e6
fernando <fernandotakeshi1@gmail.com>
2023-03-31 15:47:29 -0300

    exercicio03

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import "./App.css";
 //componentes
 import Execicio01 from "./Exercicio01/exercicio01";
 import Exercicio02 from "./Exercicio02/exercicio02";
+import Exercicio03 from "./Exercicio03/exercicio03";
 
 function App() {
   return (
     <div className="App">
       <Execicio01 />
       <Exercicio02 />
+      <Exercicio03 />
     </div>
   );
 }
---
+++ src/Exercicio03/exercicio03.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const Exercicio03 = () => {
+  const [dados, setDados] = useState({
+    nome1: "",
+    nome2: "",
+    idade1: 0,
+    idade2: 0,
+  });
+
+  const media = (dados.idade1 + dados.idade2) / 2;
+
+  return (
+    <div>
+      <h2>Exercício03</h2>
+      <form>
+        <input
+          type="text"
+          onChange={(e) => setDados((x) => ({ ...x, nome1: e.target.value }))}
+          placeholder="Digite a nome 2"
+        />
+        <input
+          type="text"
+          onChange={(e) => setDados((x) => ({ ...x, nome2: e.target.value }))}
+          placeholder="Digite a nome 2"
+        />
+        <input
+          type="number"
+          onChange={(e) =>
+            setDados((x) => ({ ...x, idade1: Number(e.target.value) }))
+          }
+          placeholder="Digite a idade 1"
+        />
+        <input
+          type="number"
+          onChange={(e) =>
+            setDados((x) => ({ ...x, idade2: Number(e.target.value) }))
+          }
+          placeholder="Digite a idade 2"
+        />
+        <br />
+        <textarea
+          rows={4}
+          cols={50}
+          value={
+            media <= 0 || !dados.nome1 || !dados.nome2
+              ? ""
+              : `A idade média de ${dados.nome1} e ${
+                  dados.nome2
+                } é de ${media.toFixed(1)} anos`
+          }
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Exercicio03;
